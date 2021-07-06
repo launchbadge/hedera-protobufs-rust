c63a1d7866b6f91d3ec89b267e726746c3062903
leah <leah@launchbadge.com>
2021-07-06 14:51:58 -0700

    feat: remodeled to reflect Kabuto's rust SDK format

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,1 @@
-// Include the `items` module, which is generated from items.proto.
-pub mod basic_types {
-    tonic::include_proto!("proto");
-}
-
-pub mod consensus_create_topic {
-    tonic::include_proto!("proto");
-}
+pub mod proto;

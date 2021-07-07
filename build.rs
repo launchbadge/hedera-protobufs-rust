4d47b678b22d178891db52923efc459b3e5c627a
Ryan Leckey <ryan@launchbadge.com>
2021-07-07 11:25:08 -0700

    fix: move proto to sub-module and refactor how protos are generated

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
 use std::env;
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, read_dir};
 use std::path::Path;
 
 fn main() -> anyhow::Result<()> {
-    tonic_build::configure()
+    // collect every proto file in services/_
+
+    let services_protos: Vec<_> = read_dir("./proto/services")?
+        .filter_map(|entry| Some(entry.ok()?.path()))
+        .collect();
+
+    // services
+
+    let mut config = tonic_build::configure()
+        .build_server(true)
         .type_attribute("proto.Key", "#[derive(Eq, Hash)]")
         .type_attribute("proto.Key.key", "#[derive(Eq, Hash)]")
         .type_attribute("proto.KeyList", "#[derive(Eq, Hash)]")
@@ -11,39 +20,57 @@ fn main() -> anyhow::Result<()> {
         .type_attribute("proto.TransactionID", "#[derive(Eq, Hash)]")
         .type_attribute("proto.AccountID", "#[derive(Eq, Hash)]")
         .type_attribute("proto.Timestamp", "#[derive(Eq, Hash)]")
-        .type_attribute("proto.ContractID", "#[derive(Eq, Hash)]")
-        .type_attribute("proto.ResponseCodeEnum", "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"SCREAMING_SNAKE_CASE\")]")
-        .compile(
-            &[
-                "proto/TransactionRecord.proto",
-                "proto/Transaction.proto",
-                "proto/TransactionBody.proto",
-                "proto/TransactionContents.proto",
-                "proto/CryptoGetAccountBalance.proto",
-            ],
-            &["proto/"],
-        )?;
+        .type_attribute("proto.ContractID", "#[derive(Eq, Hash)]");
+
+    if cfg!(feature = "serde") {
+        config = config
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .type_attribute(
+                "proto.ResponseCodeEnum",
+                "#[serde(rename_all = \"SCREAMING_SNAKE_CASE\")]",
+            );
+    }
+
+    config.compile(&services_protos, &[Path::new("proto/services/").to_owned()])?;
+
+    // mirror
+    // NOTE: must be compiled in a separate folder otherwise it will overwrite the previous build
 
     let mirror_out_dir = Path::new(&env::var("OUT_DIR")?).join("mirror");
     create_dir_all(&mirror_out_dir)?;
 
     tonic_build::configure()
         .build_server(true)
-        .extern_path(".proto.Timestamp", "crate::proto::Timestamp")
-        .extern_path(".proto.TopicID", "crate::proto::TopicId")
-        .extern_path(".proto.ConsensusMessageChunkInfo", "crate::proto::ConsensusMessageChunkInfo")
+        .extern_path(".proto.Timestamp", "crate::services::Timestamp")
+        .extern_path(".proto.TopicID", "crate::services::TopicId")
+        .extern_path(
+            ".proto.ConsensusMessageChunkInfo",
+            "crate::services::ConsensusMessageChunkInfo",
+        )
         .out_dir(&mirror_out_dir)
-        .compile(&["proto/mirror/ConsensusService.proto"], &["proto/mirror/", "proto/"])?;
+        .compile(
+            &["proto/mirror/ConsensusService.proto"],
+            &["proto/mirror/", "proto/services/"],
+        )?;
+
+    // streams
+    // NOTE: must be compiled in a separate folder otherwise it will overwrite the previous build
+
+    // FIXME: the streams module is building way too much, its generating code for a large chunk of types
+    //        from the services module
 
     let streams_out_dir = Path::new(&env::var("OUT_DIR")?).join("streams");
     create_dir_all(&streams_out_dir)?;
 
     tonic_build::configure()
-        .extern_path(".proto.Timestamp", "crate::proto::Timestamp")
-        .extern_path(".proto.AccountID", "crate::proto::AccountId")
-        .extern_path(".proto.TokenID", "crate::proto::TokenId")
+        .extern_path(".proto.Timestamp", "crate::services::Timestamp")
+        .extern_path(".proto.AccountID", "crate::services::AccountId")
+        .extern_path(".proto.TokenID", "crate::services::TokenId")
         .out_dir(&streams_out_dir)
-        .compile(&["proto/streams/AccountBalanceFile.proto"], &["proto/streams/", "proto/"])?;
+        .compile(
+            &["proto/streams/AccountBalanceFile.proto"],
+            &["proto/streams/", "proto/services/"],
+        )?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
-pub mod proto;
+#![allow(non_camel_case_types)]
+#![allow(clippy::default_trait_access, clippy::doc_markdown)]
+
+pub mod services {
+    tonic::include_proto!("proto");
+}
+
+pub mod mirror {
+    tonic::include_proto!("mirror/com.hedera.mirror.api.proto");
+}
+
+pub mod streams {
+    tonic::include_proto!("streams/proto");
+}

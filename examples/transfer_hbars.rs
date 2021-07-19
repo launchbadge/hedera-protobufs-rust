a18ec9bd452628c6b45611aa1cfb31069f80cc4b
David Lindner <davidlindner@pop-os.localdomain>
2021-07-19 15:31:44 -0700

    Ripped out create_account stuff

--- examples/transfer_hbars.rs
+++ examples/transfer_hbars.rs
@@ -20,13 +20,14 @@ async fn main() -> anyhow::Result<()> {
 
     let mut client = CryptoServiceClient::new(channel);
 
-    let my_account_id = &env::var("TESTNET_ID")?[4..10];
+    let account_id = &env::var("TESTNET_ID")?[4..10];
+    let new_account_id = 2139523;
+
     let my_account_private_key = PrivateKey::from_str(&env::var("TESTNET_PRIVATE_KEY")?)?;
     let my_account_public_key = my_account_private_key.public_key().to_bytes();
 
-    // Generated Keypair for New Account
-    let new_account_private_key = PrivateKey::generate();
-    let new_account_public_key= new_account_private_key.public_key().to_bytes();
+    let new_account_private_key = [41, 107, 201, 102, 6, 19, 57, 84, 38, 112, 7, 118, 113, 57, 127, 150, 187, 146, 121, 192, 178, 52, 183, 23, 119, 175, 244, 207, 154, 79, 229, 203];
+    let new_account_public_key  = [240, 217, 84, 212, 186, 42, 57, 229, 27, 90, 0, 28, 194, 215, 60, 221, 97, 227, 1, 218, 42, 166, 79, 167, 218, 107, 73, 148, 174, 231, 5, 245];
 
     // Define and populate the structs for create account
     let node_account_id = services::AccountId {
@@ -35,119 +36,21 @@ async fn main() -> anyhow::Result<()> {
         account_num: 3,
     };
 
-    let account_id = services::AccountId {
+    let my_account_id = services::AccountId {
         shard_num: 0,
         realm_num: 0,
-        account_num: my_account_id.parse::<i64>()?,
-    };
-
-    // 90 day duration
-    let duration = services::Duration { seconds: 7776000 };
-
-    let data = services::transaction_body::Data::CryptoCreateAccount(
-        services::CryptoCreateTransactionBody {
-            key: Some(services::Key {
-                key: Some(services::key::Key::Ed25519(new_account_public_key.to_vec())),
-            }),
-            auto_renew_period: Some(duration),
-            ..Default::default()
-        },
-    );
-
-    let now = OffsetDateTime::now_utc();
-    let now_in_seconds = now.unix_timestamp() as i64;
-    let now_in_nanos = now.nanosecond() as i32;
-
-    let transaction_id = services::TransactionId {
-        transaction_valid_start: Some(services::Timestamp {
-            seconds: now_in_seconds,
-            nanos: now_in_nanos,
-        }),
-        account_id: Some(account_id.clone()),
-        scheduled: false,
-    };
-
-    let new_account = services::TransactionBody {
-        transaction_id: Some(transaction_id.clone()),
-        node_account_id: Some(node_account_id.clone()),
-        transaction_fee: 200000000,
-        transaction_valid_duration: Some(services::Duration { seconds: 120 }),
-        memo: String::new(),
-        data: Some(data),
-        ..Default::default()
-    };
-
-    // serialize account struct
-    let account_bytes = new_account.encode_to_vec();
-
-    // sign the bytes
-    let signature = my_account_private_key.sign(&account_bytes);
-
-    // creates signed transaction so the account can be created
-    let sig_pair = vec![services::SignaturePair {
-        pub_key_prefix: my_account_public_key.to_vec(),
-        signature: Some(services::signature_pair::Signature::Ed25519(
-            signature.to_vec(),
-        )),
-    }];
-
-    let signed_transaction = services::SignedTransaction {
-        body_bytes: account_bytes,
-        sig_map: Some(services::SignatureMap { sig_pair: sig_pair }),
-    };
-
-    // construct a transaction
-    let transaction = services::Transaction {
-        signed_transaction_bytes: signed_transaction.encode_to_vec(),
-        ..Default::default()
-    };
-
-    // get response to creating the account
-    let response = client.create_account(transaction).await?;
-
-    // view account
-    let t_response = Some(response);
-    println!("{:#?}", t_response);
-
-    let query = services::Query {
-        query: Some(services::query::Query::TransactionGetReceipt(
-            services::TransactionGetReceiptQuery {
-                header: None,
-                transaction_id: Some(transaction_id.clone()),
-                include_duplicates: false,
-            },
-        )),
-    };
-
-    // wait for consensus
-    tokio::time::sleep(Duration::from_secs(15)).await;
-
-    // query the receipt
-    let response= client.get_transaction_receipts(query).await?;
-    
-    let new_account_num = match response.into_inner().response {
-        Some(services::response::Response::TransactionGetReceipt(response)) => {
-            //response.receipt,
-            match response.receipt {
-                Some(response) => {
-                    match response.account_id {
-                        Some(response) => response.account_num,
-                        _ => unreachable!(),
-                    }
-                }
-                _ => unreachable!(),
-            }
-        }
-        _ => unreachable!(),
+        account_num: account_id.parse::<i64>()?,
     };
 
     let new_account_id = services::AccountId {
         shard_num: 0,
         realm_num: 0,
-        account_num: new_account_num as i64,
+        account_num: new_account_id,
     };
 
-    println!("New Account number Account Number = 0.0.{:#?} ", new_account_num);
+    let now = OffsetDateTime::now_utc();
+    let now_in_seconds = now.unix_timestamp() as i64;
+    let now_in_nanos = now.nanosecond() as i32;
 
     // check the new account's balance
     let query = services::Query {
@@ -157,7 +60,7 @@ async fn main() -> anyhow::Result<()> {
                 balance_source: Some(
                     services::crypto_get_account_balance_query::BalanceSource::AccountId(
                         services::AccountId {
-                            account_num: new_account_num as i64,
+                            account_num: new_account_id.account_num,
                             shard_num: 0,
                             realm_num: 0,
                         },
@@ -173,15 +76,15 @@ async fn main() -> anyhow::Result<()> {
         Some(services::response::Response::CryptogetAccountBalance(response)) => response,
         _ => unreachable!(),
     };
-    println!("The new account's balance is: {} tℏ", response.balance);
+    println!("The new account's balance before transfer is: {} tℏ", response.balance);
 
-    // transfer hbar
+    // Transfer hbar
     let transfer_transaction_data = services::transaction_body::Data::CryptoTransfer(
         services::CryptoTransferTransactionBody {
             transfers: Some(services::TransferList {
                 account_amounts: vec![
                 services::AccountAmount {
-                    account_id: Some(account_id.clone()),
+                    account_id: Some(my_account_id.clone()),
                     amount: -1000,
                 },
                 services::AccountAmount {
@@ -193,17 +96,17 @@ async fn main() -> anyhow::Result<()> {
         }
     );
 
-    let transfer_transaction_id = services::TransactionId {
+    let transaction_id = services::TransactionId {
         transaction_valid_start: Some(services::Timestamp {
-            seconds: now_in_seconds + 1,
+            seconds: now_in_seconds,
             nanos: now_in_nanos,
         }),
-        account_id: Some(account_id.clone()),
+        account_id: Some(my_account_id.clone()),
         scheduled: false,
     };
 
     let transfer_transaction_body = services::TransactionBody {
-        transaction_id: Some(transfer_transaction_id.clone()),
+        transaction_id: Some(transaction_id.clone()),
         node_account_id: Some(node_account_id.clone()),
         transaction_fee: 200000000,
         transaction_valid_duration: Some(services::Duration { seconds: 120 }),
@@ -215,7 +118,7 @@ async fn main() -> anyhow::Result<()> {
     let transfer_account_bytes = transfer_transaction_body.encode_to_vec();
 
     let transfer_signature = my_account_private_key.sign(&transfer_account_bytes);
-    let receive_signature = new_account_private_key.sign(&transfer_account_bytes);
+    let receive_signature = PrivateKey::from_bytes(&new_account_private_key)?.sign(&transfer_account_bytes);
     
     let transfer_sig_pair = vec![
         services::SignaturePair {
@@ -250,7 +153,7 @@ async fn main() -> anyhow::Result<()> {
         query: Some(services::query::Query::TransactionGetReceipt(
             services::TransactionGetReceiptQuery {
                 header: None,
-                transaction_id: Some(transfer_transaction_id.clone()),
+                transaction_id: Some(transaction_id.clone()),
                 include_duplicates: false,
             },
         )),
@@ -269,7 +172,7 @@ async fn main() -> anyhow::Result<()> {
                 balance_source: Some(
                     services::crypto_get_account_balance_query::BalanceSource::AccountId(
                         services::AccountId {
-                            account_num: new_account_num,
+                            account_num: new_account_id.account_num,
                             shard_num: 0,
                             realm_num: 0,
                         },
@@ -289,7 +192,7 @@ async fn main() -> anyhow::Result<()> {
         _ => unreachable!(),
     };
 
-    println!("The new account balance after transfer is: {} tℏ", new_account_balance_response.balance);
+    println!("The new account's balance after transfer is: {} tℏ", new_account_balance_response.balance);
 
     Ok(())
 }
